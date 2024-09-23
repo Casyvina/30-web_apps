ea654c592aa460b364477ba4dcacdf10f38f2570
casyvina <josephagwuh@gmail.com>
2024-09-23 08:50:48 -0700

    2nd project

--- project-sandbox/src/projects/components/Title.jsx
+++ project-sandbox/src/projects/components/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Title({ text, classes }) {
   return (
-    <h1 className={!classes ? 'title text-center' : classes }>{!text ? "Title": text}</h1>
+    <h1 className={!classes ? 'title ' : classes && "title text-center" }>{!text ? "Title": text}</h1>
   )
 }
 
