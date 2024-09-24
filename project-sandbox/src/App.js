ff1ed76941477646954a618a82d691638576802c
casyvina <josephagwuh@gmail.com>
2024-09-24 01:52:50 -0700

    third project

--- project-sandbox/src/App.js
+++ project-sandbox/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import RandomizeColor from './projects/02-randomize-colors/RandomizeColor';
+import LikePhotoAp from './projects/03-like-my-photo/LikePhotoAp';
 // import EsignatureApp from './projects/01-e-signature-app/EsignatureApp';
 
 function App() {
   return (
     <div className="App">
       {/* <EsignatureApp /> */}
-      <RandomizeColor />
+      {/* <RandomizeColor /> */}
+      <LikePhotoAp />
     </div>
   );
 }
